b2b9debe6fcbacf4f2b118c85ac332001af39a13
Ben Wagaman <jamin4jc@rubicon.(none)>
2010-02-03 21:58:57 -0500

    authentication!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include Clearance::Authentication
   acts_as_iphone_controller
   layout 'default'
 
